ceedd8ae0c755948bab5c5652e84bba210aced24
Diyar.codes <diyar.codes16@gmail.com>
2020-01-19 14:48:02 +0700

    meringkas script

--- application/controllers/admin/Data_mobil.php
+++ application/controllers/admin/Data_mobil.php
@@ -31,38 +31,7 @@ class Data_mobil extends CI_Controller {
             $this->load->view('admin/form_tambah_mobil', $data);
             $this->load->view('templates_admin/footer');
         } else {
-            $kode_type = $this->input->post('kode_type');
-            $merk = $this->input->post('merk');
-            $no_plat = $this->input->post('no_plat');
-            $warna = $this->input->post('warna');
-            $tahun = $this->input->post('tahun');
-            $status = $this->input->post('status');
-            $gambar = $_files['gambar']['name'];
-            
-            if( $gambar = '') {
-
-            } else {
-                $config['upload_path'] = './assets/img/imgcar';
-                $config['allowed_types'] = 'jpg|jpeg|png|tiff';
-
-                $this->load->library('upload', $config);
-                if(!$this->upload->do_upload('gambar')) {
-                    echo "Gambar Gagal Diupload!";
-                } else {
-                    $gambar = $this->upload->data('file_name');
-                }
-            }
-            $data = [
-                'kode_type' => $kode_type,
-                'merk' => $merk,
-                'no_plat' => $no_plat,
-                'tahun' => $tahun,
-                'warna' => $warna,
-                'status' => $status,
-                'gambar' => $gambar
-            ];
-
-            $this->db->insert('mobil', $data);
+            $this->rental_model->tambahDataMobil();
             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
             Data Mobil Berhasil Ditambahkan
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
--- application/models/rental_model.php
+++ application/models/rental_model.php
@@ -12,4 +12,35 @@ class rental_model extends CI_model
     {
         return $this->db->get('type')->result_array();
     }
+
+    public function tambahDataMobil()
+    {
+        $data = [
+            'kode_type' => $this->input->post('kode_type'),
+            'merk' => $this->input->post('merk'),
+            'no_plat' => $this->input->post('no_plat'),
+            'tahun' => $this->input->post('tahun'),
+            'warna' => $this->input->post('warna'),
+            'status' => $this->input->post('status'),
+            'gambar' => $this->_uploadGambar()
+        ];
+
+        $this->db->insert('mobil', $data);
+    }
+
+    private function _uploadGambar()
+    {
+        $config['upload_path']          = './assets/img/imgcar/';
+        $config['allowed_types']        = 'jpeg|jpg|png|gif';
+        $config['max_size']             = '5000';
+
+        $this->upload->initialize($config);
+
+        if ($this->upload->do_upload('gambar')) {
+            return $this->upload->data('file_name');
+        } else {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+            redirect('admin/Data_mobil');
+        }
+    }
 }
